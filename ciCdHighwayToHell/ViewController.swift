e2289d3d5b4403a6355ddc0fda54a4443c2b93e9
pierrick <pierrickviret@gmail.com>
2024-02-27 20:52:16 +0100

    init commit

--- ciCdHighwayToHell/ViewController.swift
+++ ciCdHighwayToHell/ViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let label = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        label.text = "Highway to Hell"
+        
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
     }
 
 
